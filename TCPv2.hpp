b948b37cc44faa0f2f6dad6408b9d96eb8caa201
SoStealth <36263330+SoStealth@users.noreply.github.com>
2018-02-08 11:07:57 +0000

    Update TCPv2.hpp

--- TCPv2.hpp
+++ TCPv2.hpp
@@ -7,19 +7,35 @@
 
 #define IP_LOOPBACK "127.0.0.1"
 #define IP_MYSELF "0.0.0.0"
+#define MAX_CONN 50
 
 class Connection{
 private:  int conn_id;
 public:   Connection(int conn_id);
-          ~Connection();
+          ~Connection(); /* API: shutdown() */
           bool invia(char* msg);
-          bool invia_raw(void* buffer, int length);
+          bool invia_raw(void* buffer, int length); /* API: send() */
           char* ricevi();
-          char* ricevi_raw(int* length);
-          
+          char* ricevi_raw(int* length); /* API: recv() */
 };
-class ServerTCP;
-class ClientTCP;
+class ServerTCP{
+private:  int sock_id;
+public:   ServerTCP(int port, bool loopback); /* API: bind() */
+          ~ServerTCP(); /* API: close() */
+          Connection accetta(); /* API: accept() */
+          bool broadcast(bool broadcast); /* API: setsockopt() */
+};
+class ClientTCP{
+private:  int sock_id;
+public:   ClientTCP(bool loopback);
+          ~ClientTCP(); /* API: close() */
+          bool connetti(Address server); /* API: connect() */
+          bool invia(char* msg);
+          bool invia_raw(void* buffer, int length); /* API: send() */
+          char* ricevi();
+          char* ricevi_raw(int* length); /* API: recv() */
+          bool broadcast(bool broadcast); /* API: setsockopt() */
+};
 
 
 
